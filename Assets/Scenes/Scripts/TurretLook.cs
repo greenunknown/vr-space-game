6a7dcb3f2fe93914538936ddcac31501e33e6f72
Brandon Le <greenunknown0@gmail.com>
2020-11-08 01:47:03 -0800

    Adding turret lookat script.

---
+++ Assets/Scenes/Scripts/TurretLook.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretLook : MonoBehaviour
+{
+    public Transform target;
+    public float range;
+
+    // Start is called before the first frame update
+    void Start()
+    {    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.LookAt(target);  
+        Vector3 relativePos = target.position - transform.position;
+
+        // the second argument, upwards, defaults to Vector3.up
+        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+        transform.rotation = rotation; 
+    }
+}
