b85534d168b24cab48202a30ed6d2e19bf0e338b
Brandon Le <greenunknown0@gmail.com>
2020-11-08 02:04:49 -0800

    Adding range and modified look down limit to turret script.

--- Assets/Scenes/Scripts/TurretLook.cs
+++ Assets/Scenes/Scripts/TurretLook.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class TurretLook : MonoBehaviour
 {
     public Transform target;
-    public float range;
+    public float range = 25f;
 
+    private float turret_down_look = -1;
     // Start is called before the first frame update
     void Start()
     {    
@@ -15,11 +16,22 @@ public class TurretLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // transform.LookAt(target);  
-        Vector3 relativePos = target.position - transform.position;
-
-        // the second argument, upwards, defaults to Vector3.up
-        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
-        transform.rotation = rotation; 
+        // transform.LookAt(target); 
+        if ((target.position.x - transform.position.x) < range && (target.position.z - transform.position.z) < range) 
+        {
+            if ((target.position.y - transform.position.y) > turret_down_look) 
+            {
+                Vector3 relativePos = target.position - transform.position;
+                Debug.Log("relativePos: " + relativePos + " tary - tray: " + (target.position.y - transform.position.y));
+                // the second argument, upwards, defaults to Vector3.up
+                Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+                transform.rotation = rotation; 
+            }
+        } 
+        // Vector3 relativePos = target.position - transform.position;
+        // Debug.Log("relativePos: " + relativePos + " tary - tray: " + (target.position.y - transform.position.y));
+        // // the second argument, upwards, defaults to Vector3.up
+        // Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+        // transform.rotation = rotation; 
     }
 }
