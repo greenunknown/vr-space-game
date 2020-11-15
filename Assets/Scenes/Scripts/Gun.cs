bd7cf017a3bd1e4365ad96d960b4b5870e20809b
Brandon Le <greenunknown0@gmail.com>
2020-11-14 21:02:52 -0800

    Adding muzzle effect when shooting and hit effect when ray hits an object.

--- Assets/Scenes/Scripts/Gun.cs
+++ Assets/Scenes/Scripts/Gun.cs
@@ -6,15 +6,21 @@ public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float fireRate = 2f; // Lower --> Slower, Higher --> Faster
+    public float impactForce = 30f;
 
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")) 
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire) 
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
         
@@ -34,6 +40,14 @@ public class Gun : MonoBehaviour
             {
                 target.TakeDamage(damage);
             }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 1f);
         }
     }
 }
