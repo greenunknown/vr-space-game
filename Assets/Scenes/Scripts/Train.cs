0c0f870be80e75fd9647e9f58704506b75b756ba
Brandon Le <greenunknown0@gmail.com>
2020-11-14 22:20:43 -0800

    Adding movement logic for train. Train model has issues with its forward direction so will need to fix that. Currently using a elongated box as concept.

--- Assets/Scenes/Scripts/Train.cs
+++ Assets/Scenes/Scripts/Train.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Train : MonoBehaviour
 {
+    public float trainSpeed = 5f;
+    public float initialPosition;
+    public float currentDistance = 0f;
+    public float maxDistance = 100f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPosition = transform.position.z;
+        // Debug.Log("Inital position: " + initialPosition);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (currentDistance < maxDistance)
+        {
+            currentDistance = transform.position.z - initialPosition; 
+            // Debug.Log("Current position: " + transform.position.z);
+            transform.position += transform.forward * trainSpeed * Time.deltaTime;
+        }
         
     }
 }
